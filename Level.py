2a33f2826e17c18ac2ebc7f12e85e892fcd45b47
joeylmaalouf <joeylmaalouf@gmail.com>
2015-04-09 14:32:28 -0400

    more level stuff

--- Level.py
+++ Level.py
@@ -2,6 +2,12 @@ class Level(object):
 	""" The Level object, representing
 		the state of the game board.
 	"""
-	def __init__(self, grid):
+	def __init__(self, grid, goal):
 		super(Level, self).__init__()
 		self.contents = grid
+		self.end = goal
+
+
+if __name__ == "__main__":
+	grid = [[0, 0, 1], [-1, 0, 1], [0, 1, 2]]
+	lvl1 = new Level(grid, (2, 2))
